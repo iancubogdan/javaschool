fc98140e13b16b4d8d0986c4c9505ee8d4e6d9be
iancubogdan91@yahoo.com <extreme11>
2017-03-08 15:12:55 +0200

    added Playground

---
+++ src/test/java/com/db/javaschool/springdata/Playground.java
@@ -0,0 +1,12 @@
+package com.db.javaschool.springdata;
+
+
+import com.db.javaschool.springdata.config.AppConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class Playground {
+}
